f899b2623dd7d70d7f84dfc0e25612b7e4406db3
Audrea Cook <audreacwork@gmail.com>
2019-10-21 17:11:43 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,79 @@
-const addBtn = document.querySelector('#new-toy-btn')
-const toyForm = document.querySelector('.container')
-let addToy = false
+const addBtn = document.querySelector('#new-toy-btn');
+const toyForm = document.querySelector('.container');
+let addToy = false;
 
-// YOUR CODE HERE
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('Call function to fetch toys now');
+  renderToys()
+
+});
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
-  addToy = !addToy
+  addToy = !addToy;
   if (addToy) {
     toyForm.style.display = 'block'
   } else {
     toyForm.style.display = 'none'
   }
-})
+});
+
+const form = document.querySelector('.add-toy-form');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log('form submitted');
+  let toyData = {
+    name: form.querySelector('input[name]').value,
+    image: form.querySelector('input[name="image"]').value,
+    likes: 0
+  };
+  console.log(toyData)
+
+  let configObj = {
+    method: 'POST',
+    headers: {
+      "Content-Type": 'application/json',
+      "Accept": 'application/json'
+    },
+    body: JSON.stringify(toyData)
+  };
+
+  fetch('http://localhost:3000/toys', configObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        console.log(object);
+      })
+      .catch(function (error) {
+        alert("Someone's coming!");
+        console.log(error.message)
+      })
+});
+
+function createToyCards(object) {
+    for (const element of object) {
+      for (const key in element) {
+        console.log(element[key])
+      }
+    }
+}
+
+function renderToys() {
+  fetch('http://localhost:3000/toys')
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        createToyCards(object);
+        console.log(object);
+      })
+      .catch(function (error) {
+        alert("Someone's coming!");
+        console.log(error.message)
+      })
+}
 
 
-// OR HERE!
+
