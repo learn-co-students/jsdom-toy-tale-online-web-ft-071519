34a976d86140f7b15096354cca5f42726c8e1fd6
Nicole Dow <nikki.dow71@gmail.com>
2019-10-23 13:45:30 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const toyList = document.getElementById('toy-collection')
 let addToy = false
 
-// YOUR CODE HERE
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -14,5 +14,37 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+document.addEventListener('DOMContentLoaded', fetchToys());
 
-// OR HERE!
+function fetchToys() {
+  return fetch('http://localhost:3000/toys')
+    .then(response => response.json())
+    .then(json => loadToys(json))
+}
+
+function loadToys(json) {
+  json.forEach(toy => {
+    const div = document.createElement('div')
+    div.className = 'card'
+
+    const h2 = document.createElement('h2')
+    h2.innerText = `${toy.name}`
+
+    const img = document.createElement('img')
+    img.src = `${toy.image}`
+    img.className = 'toy-avatar'
+
+    const p = document.createElement('p')
+    p.innerText = `${toy.likes} likes`
+
+    const button = document.createElement('button')
+    button.className = 'like-btn'
+    button.innerText = 'Like'
+
+    toyList.appendChild(div)
+    div.appendChild(h2)
+    div.appendChild(img)
+    div.appendChild(p)
+    div.appendChild(button)
+  })
+}
